3d6f91336e66050d8871847567beb289cfb38dad
maikerosa <maikeofc18@gmail.com>
2023-01-12 20:48:14 -0300

    tasks

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\UserController as UserController;
 // view
 Route::get('/', [ViewController::class, 'index']);
 Route::match(['get', 'delete'],'/users', [ViewController::class, 'users'])->name('users');
+Route::get('/tasks', [ViewController::class, 'tasks']);
 Route::match(['get', 'post'],'/users/new', [ViewController::class, 'new_user'])->name('users_edit');
 Route::get('/users/{id}', [ViewController::class, 'user_details']);
 Route::put('/users/{id}', [UserController::class, 'update_user']);
